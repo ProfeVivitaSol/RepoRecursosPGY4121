55a45409faa86f8719204db862061efa4a8c56d7
ProfeVivitaSol <profevivitasol@gmail.com>
2024-11-21 23:22:26 -0300

    Recursos varios

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit{
+
+  constructor() {}
+
+  ngOnInit(){
+    Camera.requestPermissions();
+  }
+
+  async leerQr(){
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera
+      });
+ };
+}
